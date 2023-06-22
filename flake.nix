d75538b9541ffb622694203e00f4e58b04c8c484
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-06-22 18:28:26 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.nur.url = "github:nix-community/NUR";
+  inputs.nur.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {self, nixpkgs, flake-utils, nur, ...}@inputs: 
+  let
+    recipe =  { writeShellApplication, pkgs }:
+
+      writeShellApplication rec {
+        name = "mum-rofi";
+
+        runtimeInputs = [
+          pkgs.nur.repos.lykos153.mum
+          pkgs.libnotify
+          pkgs.gnused
+          pkgs.gnugrep
+          pkgs.rofi
+        ];
+
+        text = builtins.readFile ./mum-rofi;
+      };
+  in
+  flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ nur.overlay ];
+    };
+  in {
+    defaultPackage = pkgs.callPackage recipe {};
+  });
+}
