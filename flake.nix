394a19592b26f9b240246895bc9ab1db9c02753c
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-02-07 15:05:11 +0100

    Use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,32 +1,59 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   inputs.nur.url = "github:nix-community/NUR";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = {self, nixpkgs, flake-utils, nur, ...}@inputs: 
-  let
-    recipe =  { writeShellApplication, pkgs }:
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        inputs',
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.nur.overlay
+          ];
+        };
+        overlayAttrs = {
+          inherit (config.packages) default;
+        };
+        packages = rec {
+          default = rofi-mum;
+          rofi-mum = pkgs.callPackage (
+            {
+              writeShellApplication,
+              nur,
+              libnotify,
+              gnused,
+              gnugrep,
+              rofi,
+            }:
+              writeShellApplication {
+                name = "rofi-mum";
 
-      writeShellApplication rec {
-        name = "rofi-mum";
+                runtimeInputs = [
+                  nur.repos.lykos153.mum
+                  libnotify
+                  gnused
+                  gnugrep
+                  rofi
+                ];
 
-        runtimeInputs = [
-          pkgs.nur.repos.lykos153.mum
-          pkgs.libnotify
-          pkgs.gnused
-          pkgs.gnugrep
-          pkgs.rofi
-        ];
-
-        text = builtins.readFile ./rofi-mum;
+                text = builtins.readFile ./rofi-mum;
+              }
+          ) {};
+        };
+        formatter = pkgs.alejandra;
       };
-  in
-  flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nur.overlay ];
     };
-  in {
-    packages.default = pkgs.callPackage recipe {};
-  });
 }
