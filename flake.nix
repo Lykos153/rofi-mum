bad606f7dff2fd48c9d8a94f33e6fd1286ebde4e
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-04-14 15:02:53 +0200

    remove dependency on NUR
    mum is in nixpkgs now

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.nur-packages.url = "github:lykos153/nur-packages";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs = inputs @ {flake-parts, ...}:
@@ -14,16 +13,8 @@
       perSystem = {
         config,
         pkgs,
-        system,
-        inputs',
         ...
       }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.nur-packages.overlay
-          ];
-        };
         overlayAttrs = {
           inherit (config.packages) rofi-mum;
         };
@@ -32,17 +23,17 @@
           rofi-mum = pkgs.callPackage (
             {
               writeShellApplication,
-              lykos153,
               libnotify,
               gnused,
               gnugrep,
               rofi,
+              mum,
             }:
               writeShellApplication {
                 name = "rofi-mum";
 
                 runtimeInputs = [
-                  lykos153.mum
+                  mum
                   libnotify
                   gnused
                   gnugrep
